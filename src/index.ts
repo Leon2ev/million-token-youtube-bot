54cd574fece4403d8bf744bb8fdc2c65e5d68f00
Leon2ev <jevgeni.glazunov@gmail.com>
2021-06-15 20:07:29 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import axios from 'axios'
+
+const app: Application = express()
+const port = 4000
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+}
+
+// Body parsing Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors(corsOptions))
+app.use(helmet())
+
+// Routes
+app.get('/', async (req: Request, res: Response): Promise<Response> => {
+  return res.send('Hello World')
+})
+
+app.listen(port, (): void => {
+  console.log(`Connected successfully on port ${port}`)
+})
